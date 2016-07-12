f5490ca8ba40e1dcaaf1c4f2a0f38a548f5436f6
mseefelder <saa.marcos@gmail.com>
2016-07-12 20:34:38 -0300

    Most of second assignement done

--- ex2-lua/coordinator.lua
+++ ex2-lua/coordinator.lua
@@ -1,35 +1,83 @@
+-- load dependencies
 local socket = require "socket"
 local copas = require "copas"
-local list = require "list"
+local queue = require "queue"
 
+-- this coordinator's host and port
 local port = 51034
 local server = socket.udp()
+
+-- make socket
 server:setsockname("*",port)
 
 function handler(skt)
-  skt = copas.wrap(skt)
-  --q = List.new()
-  print("UDP connection handler")
+	--use luasocket calls directly on skt
+	skt = copas.wrap(skt)
 
-  while true do
-    local s, err
-    print("receiving...")
-    s, err, sktport, sktip = skt:receivefrom(1)
-    if not s then
-      print("Receive error: ", err)
-      return
-    --elseif s==tostring(0x0) then
+	-- Queue for waiting writers
+	local q = Queue.new()
+	-- Coordinator's log
+	local file = io.open("log.txt", "w")
+	file:write("COORDINATOR LOG\n")
+	file:close()
 
-    --elseif s==tostring(0x1) then
+	print("UDP mutex coordinator")
+	while true do
+	-- auxiliar variables
+	local s, err, sktport, sktip, message
 
-    end
-    print("Received data, bytes:" , #s)
-    print("Data: ", s)
-    print("IP: ", sktip)
-    print("Port: ", sktport)
-    skt:sendto(0x2, "*", sktport)
-  end
+	-- receive message on socket
+	-- store sender infos on variables
+	print("receiving...")
+	s, err, sktport, sktip = skt:receivefrom(1)
+
+	if not s then
+		print("Receive error: ", err)
+		break
+	else
+		-- CENTRALIZED MUTEX ALGORITHM start
+			-- Received REQUEST == 0x0
+			if s==tostring(0x0) then 
+				message = "REQUEST"
+				if Queue.isempty(q) then
+				skt:sendto(0x2, "*", sktport)
+				end
+				Queue.push(q, sktport)
+
+			-- Received RELEASE == 0x1
+			elseif s==tostring(0x1) then
+				message = "RELEASE"
+				Queue.pop(q)
+				if not Queue.isempty(q) then
+				-- send GRANT == 0x2
+				skt:sendto(0x2, "*", Queue.head(q))
+				end
+
+			-- Received unknown	
+			else
+				message = "UNKNOWN"
+			end
+		-- CENTRALIZED MUTEX ALGORITHM end
+		
+		-- Write log
+		file = io.open("log.txt", "a")
+		file:write(
+		os.date() .. 
+		" Process: " .. 
+		sktport .. 
+		": " .. 
+		message ..
+		"\n"
+		)
+		file:close()
+	end
+
+	print("Data: ", s)
+	print("Port: ", sktport)
+	end
+
 end
 
+-- Create server and start serving
 copas.addserver(server, handler, 1)
 copas.loop()
