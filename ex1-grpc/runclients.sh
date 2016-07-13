4d4ed44365d4ae9791125de4e06f662f2d4efc63
mseefelder <saa.marcos@gmail.com>
2016-07-13 02:26:03 -0300

    added test automator

---
+++ ex1-grpc/runclients.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+./array_rpc_client 100000000 1 0
+./array_rpc_client 100000000 2 0
+./array_rpc_client 100000000 4 0
+./array_rpc_client 100000000 8 0
+./array_rpc_client 100000000 16 0
+./array_rpc_client 100000000 32 0
+./array_rpc_client 100000000 64 0
+./array_rpc_client 100000000 128 0
+
+./array_rpc_client 100000000 1 1
+./array_rpc_client 100000000 2 1
+./array_rpc_client 100000000 4 1
+./array_rpc_client 100000000 8 1
+./array_rpc_client 100000000 16 1
+./array_rpc_client 100000000 32 1
+./array_rpc_client 100000000 64 1
+./array_rpc_client 100000000 128 1
+
+./array_rpc_client 100000000 1 2
+./array_rpc_client 100000000 2 2
+./array_rpc_client 100000000 4 2
+./array_rpc_client 100000000 8 2
+./array_rpc_client 100000000 16 2
+./array_rpc_client 100000000 32 2
+./array_rpc_client 100000000 64 2
+./array_rpc_client 100000000 128 2
