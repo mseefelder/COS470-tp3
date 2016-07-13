b4436d7bc2e066cf66d5b7f0ff4141e35f1bf696
mseefelder <saa.marcos@gmail.com>
2016-07-13 17:51:57 -0300

    Organized for merge

--- ex2-lua-v2/runclients.sh
+++ ex2-lua-v2/runclients.sh
@@ -12,11 +12,13 @@ source ~/.bashrc
 #declare -a arr=("Bass" "Wu" "Sanchez" "Hawkins" "Valenzuela" "Hensley" "Bailey" "Carson" "Carr" "Hunt" "Stanley" "Nash" "Ibarra" "Gould" "Orr" "Hays" "Keith" "Chandler" "Paul" "Choi" "Key" "Shah" "Wong" "Ballard" "Duke" "Brady" "Miles" "Davidson" "Huber" "Pierce" "Pittman" "Avila" "Randall" "Holmes" "Wiley" "Harvey" "Terrell" "Ward" "Krueger" "Bean" "Stein" "Moon" "Le" "Ochoa" "Moyer" "Mcclure" "Walters" "Mays" "Downs" "Harding" "Merritt" "Mcgee" "Lynn" "Short" "Kemp" "Oneill" "Erickson" "Barber" "Carrillo" "Duncan" "Morales" "Reid" "Noble" "Bernard")
 declare -a arr=("Bass" "Wu" "Sanchez" "Hawkins" "Valenzuela" "Hensley" "Bailey" "Carson" "Carr" "Hunt" "Stanley" "Nash" "Ibarra" "Gould" "Orr" "Hays" "Keith" "Chandler" "Paul" "Choi" "Key" "Shah" "Wong" "Ballard" "Duke" "Brady" "Miles" "Davidson" "Huber" "Pierce" "Pittman" "Avila" "Randall" "Holmes" "Wiley" "Harvey" "Terrell" "Ward" "Krueger" "Bean" "Stein" "Moon" "Le" "Ochoa" "Moyer" "Mcclure" "Walters" "Mays" "Downs" "Harding" "Merritt" "Mcgee" "Lynn" "Short" "Kemp" "Oneill" "Erickson" "Barber" "Carrillo" "Duncan" "Morales" "Reid" "Noble" "Bernard" "Brewer" "Vega" "Campos" "Jones" "Marquez" "Frederick" "Watts" "Benjamin" "Rodriguez" "Barr" "Luna" "Coleman" "Kramer" "Lopez" "Bentley" "Macias" "Evans" "Salinas" "Copeland" "Payne" "Watkins" "Mcgrath" "Mack" "Carpenter" "Roach" "Harrell" "Navarro" "James" "Simmons" "Haley" "Zuniga" "Page" "Hurst" "Donovan" "Patterson" "Cervantes" "Doyle" "Montoya" "Velez" "Yu" "May" "Berger" "Day" "Dominguez" "West" "Wilkerson" "Reeves" "Mcguire" "Ellison" "Munoz" "Jefferson" "Bautista" "Gardner" "Dennis" "Wall" "Morse" "Cook" "Mccullough" "Duran" "Thomas" "Meyers" "Wright" "Daugherty" "Barker")
 
-/lcg/home/mseefelder/workspace/resources/lua-5.3.3/install/bin/lua coordinator.lua &
+lua coordinator.lua &
 
 ## now loop through the above array
 for i in "${arr[@]}"
 do
    echo "$i"
-   /lcg/home/mseefelder/workspace/resources/lua-5.3.3/install/bin/lua client.lua "$i" $RANDOM &
+   lua client.lua "$i" $RANDOM &
 done
+
+wait
